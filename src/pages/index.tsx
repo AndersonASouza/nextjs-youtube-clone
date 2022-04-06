23a99b874aa6ab24fa69f28e2b9f8d11ce93fb72
AndersonASouza <andersonandredesouza01@gmail.com>
2022-04-05 21:54:47 -0300

    Config Eslint + Prettier + EditorConfig

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      Clone youtube com Next.js + Material UI
+      <div>teste</div>
+    </div>
+  )
+}
+
+export default Home
